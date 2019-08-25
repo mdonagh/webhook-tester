e9ec58d826973ffa3b6ec85020f7addbe82c37c0
Mark <mdonagh@icloud.com>
2019-08-25 13:44:13 -0700

    request body

--- db/migrate/20190825202832_create_webhooks.rb
+++ db/migrate/20190825202832_create_webhooks.rb
@@ -2,6 +2,7 @@ class CreateWebhooks < ActiveRecord::Migration[5.2]
   def change
     create_table :webhooks do |t|
        t.string :request
+       t.string :request_body
 
       t.timestamps
     end
